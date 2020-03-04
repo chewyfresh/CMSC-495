735003c30920e9b14ec4d85f18643ca83d2a7f38
Lesly Brisson <59904689+lbrsn@users.noreply.github.com>
2020-03-04 15:32:22 -0800

    1st Draft: Fitness Classes & Fitness Goal Class

---
+++ WorkingDocs/FitnessGoals.py
@@ -0,0 +1,7 @@
+class FitnessGoals:
+	def __init__(self, weight_lost=None, resting_heartrate=None,sessions_completed=None,calories_burned=None, goal=None)
+	self.weight_lost = weight_lost
+	self.resting_heartrate = resting_heartrate
+	self.sessions_completed = sessions_completed
+	self.calories_burned = calories_burned
+	self.goal = goal
